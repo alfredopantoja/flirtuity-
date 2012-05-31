57689c11016cae68ae30b90f225f5ab2e8fa5ee8
Alfredo Pantoja <chololoy@gmail.com>
2012-05-31 04:27:47 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flirtuity::Application.config.secret_token = '17ebac3213e6d2ec2a386adba9d9a687189ffb13cbd76fa58c5f575be442c9d72499b83c1095e73ca513f2b048dcf35731061d4a3cafcd8657e1f14656a29940'
